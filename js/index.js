e888c1c888ea451a7e0d85877a08553ddb9dfc2a
alexboii <potteref@gmail.com>
2017-05-28 21:55:06 -0400

    Added dynamic author, changed background and font, redesigned

--- js/index.js
+++ js/index.js
@@ -18,6 +18,8 @@ function readTextFile(file) {
 }
 
 var div = document.getElementById("poem");
+var div2 = document.getElementById("poem-info");
+
 
 function appendPoem(poemArray) {
     poemArray.forEach(line => {
@@ -27,12 +29,23 @@ function appendPoem(poemArray) {
     });
 }
 
+function displayTitle(title, author) {
+    var titleHeader = document.createElement('h1');
+    var authorHeader = document.createElement('h3');
+    titleHeader.innerHTML = Boolean(title) ? title : "Unknown title";
+    authorHeader.innerHTML = Boolean(author) ? author : "Unknown author";
+    div2.appendChild(titleHeader);
+    div2.appendChild(authorHeader);
+}
+
+
 allFiles = readTextFile("js/files.txt").split("\n").map(obj => obj.substr(1));
 var randomPoemDir = "js/collection" + allFiles[Math.floor(Math.random() * allFiles.length)];
-var poemJSON = readTextFile(randomPoemDir);
+// var poemJSON = readTextFile(randomPoemDir);
 
 $.getJSON(randomPoemDir, function(json) {
     poemJSON = json; // this will show the info it in firebug console
     appendPoem(poemJSON.text);
+    displayTitle(poemJSON.title, poemJSON.author);
     console.log(poemJSON);
 });
